5790f1937f000deabccb3a95601a74502b488da0
mkg101 <manishkumargupta875@gmail.com>
2023-03-26 16:04:41 +0530

    about section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Navbar } from "./components";
 import { Header } from "./containers";
+import { About } from "./containers";
 import "./App.scss";
 function App() {
   return (
     <div className="app">
       <Navbar />
       <Header />
+      <About />
     </div>
   );
 }
